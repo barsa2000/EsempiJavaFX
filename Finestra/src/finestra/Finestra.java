ecf7b2d9ed9223b3bf2d3995e02eeec57afba83f
barsa2000 <barsanti-filippo2@hotmail.it>
2018-02-13 17:46:56 +0100

    Aggiunto primo esempio contenente una classe finestra di base

---
+++ Finestra/src/finestra/Finestra.java
@@ -0,0 +1,33 @@
+package finestra;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Finestra extends Application{
+    
+    final int h = 300, w = 300;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Registration Form");
+        
+        Button button = new Button("cliccami");
+        
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(button);
+        
+        Scene scene = new Scene(layout, h, w);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String args[]){
+        launch(args);
+    }
+    
+}
